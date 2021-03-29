61afdcbb03b8b5c1c1da07f0631511cef98f4018
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-03-29 15:17:40 +0200

    Created interface of Advanced math operations.

--- src/main/java/com/assignment/calculator/examination_calculator/AdvancedMathOperationsInterface.java
+++ src/main/java/com/assignment/calculator/examination_calculator/AdvancedMathOperationsInterface.java
@@ -1,5 +1,10 @@
 package com.assignment.calculator.examination_calculator;
 
 public interface AdvancedMathOperationsInterface {
+	public double square(double number);
+	public double squareRoot(double number);
+	public double absoluteValue(double number);
+	public double raisedToThePowerof(double base, double exponent);
+	public double modulus(double dividend, double divisor);
 
 }
