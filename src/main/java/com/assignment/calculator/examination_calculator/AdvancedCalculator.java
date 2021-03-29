d434f82a6876847d444d87fc5aae46aacecf421f
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-03-29 22:52:15 +0200

    Created class Advanced calculator.

--- src/main/java/com/assignment/calculator/examination_calculator/AdvancedCalculator.java
+++ src/main/java/com/assignment/calculator/examination_calculator/AdvancedCalculator.java
@@ -1,5 +1,31 @@
 package com.assignment.calculator.examination_calculator;
 
-public class AdvancedCalculator {
+public class AdvancedCalculator extends BasicCalculator implements AdvancedMathOperationsInterface{
+
+	
+	public double square(double number) {
+		double result = number * number;
+		return result;
+	}
+
+	public double squareRoot(double number) {
+		double result = Math.sqrt(number);
+		return result;
+	}
+
+	public double absoluteValue(double number) {
+		double result = Math.abs(number);
+		return result;
+	}
+
+	public double raisedToThePowerof(double base, double exponent) {
+		double result = Math.pow (base, exponent);
+		return result;
+	}
+
+	public double modulus(double dividend, double divisor) {
+		double result = dividend % divisor;
+		return result;
+	}
 
 }
