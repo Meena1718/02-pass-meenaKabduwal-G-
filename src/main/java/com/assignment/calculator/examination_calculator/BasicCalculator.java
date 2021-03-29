07f6e4a76cf71ba38a08d01dbf352bb6e1c0aef8
Meena Kabduwal <kabduwalmeena@gmail.com>
2021-03-29 16:05:52 +0200

    Created Basic calculator class.

--- src/main/java/com/assignment/calculator/examination_calculator/BasicCalculator.java
+++ src/main/java/com/assignment/calculator/examination_calculator/BasicCalculator.java
@@ -1,5 +1,32 @@
 package com.assignment.calculator.examination_calculator;
 
-public class BasicCalculator {
+public class BasicCalculator implements BasicMathOperationsInterface {
+
+
+	public double addition(double firstNumber, double secondNumber) {
+		double result = firstNumber + secondNumber;
+		return result;
+	}
+
+	public double subtractiont(double firstNumber, double secondNumber) {
+		double result = firstNumber - secondNumber;
+		return result;
+    }
+
+	public double multiplication(double firstNumber, double secondNumber) {
+		double result = firstNumber * secondNumber;
+		return result;
+	}
+
+	public double division(double firstNumber, double secondNumber) {
+		if (secondNumber == 0) {
+		System.out.println("You can not divide by zero");
+		return -0.321456;
+		
+	}else {
+		double result = firstNumber / secondNumber; 
+		return result;
+	}
+  }
 
 }
